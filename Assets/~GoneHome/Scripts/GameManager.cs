9af0660b655a6b487a68d5eddfe5f1a146fca2f5
RowanLove <rowanlove207@gmail.com>
2018-03-16 15:54:59 +1100

    Started new game "SunnyLand"

---
+++ Assets/~GoneHome/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+namespace GoneHome
+{
+    public class GameManager : MonoBehaviour
+    {
+        //swtiches to next scene when called
+        public void NextLevel()
+        {
+            //get the current loaded scene
+            Scene currentScene = SceneManager.GetActiveScene();
+            //load the one next to it (using build index)
+            SceneManager.LoadScene(currentScene.buildIndex + 1);
+        }
+
+        public void ResetLevel()
+        {
+            Scene currentScene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(currentScene.buildIndex);
+        }
+    }
+}
---
+++ Assets/~GoneHome/Scripts/Goal.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.Events;
+namespace GoneHome
+{
+    public class Goal : MonoBehaviour
+    {
+        public UnityEvent onTriggered;
+     
+        //runs function when collider enters triger (Goal) 
+        void OnTriggerEnter(Collider other)
+        {
+            //check if other is "Player"
+            if (other.name == "Player")
+            {
+                // run 'onTriggered' event
+
+                onTriggered.Invoke();
+            }
+        }
+    }
+}
